1bc275c69a4a7bc017bc3f5ebc8013b757d5b12d
czm2159 <286389439@qq.com>
2019-04-01 22:04:26 +0800

    JSON.stringify的修改

--- 中高级前端必备手写技能.js
+++ 中高级前端必备手写技能.js
@@ -18,7 +18,7 @@ function New(Constructor,...args){
 /*
 二、实现JSON.stringify
 	1.如果属性的值是undefined,symbol和function的话，直接忽略该属性；如果这些值出现在数组中，则转为null
-	2.如果出现循环引用，应该会报错，导致得不到结果，这里不做处理
+	2.如果出现循环引用，应该会报错，导致得不到结果，这里处理为null
 	3.如果值是对象，数组或者日期，且有toJSON方法，则返回toJson()方法相应的返回值，如果没有返回值，则忽略
 	3.只会处理对象自身的可枚举属性，所以用Object.entries最合适
 		for...in：自身和原型中的可枚举属性，不包括Symbol
@@ -28,35 +28,49 @@ function New(Constructor,...args){
 		Reflect.ownKeys：自身的所有属性（可枚举，不可枚举，Symbol）
 */
 function JSONStringify(obj){
-    let type = typeof obj;
-    //如果是值类型，直接返回字符串形式
-    if(type !== 'object' && type !== 'function') return (type === 'string')?`"${obj}"`:`${obj}`;
-    //定义结果对象
-    let res = [], _origin = obj;
-    //是否是数组类型
-    let isArray = Object.prototype.toString.call(obj).slice(8,-1) === 'Array';
-    //循环自身可枚举键值对
-    Object.entries(obj).forEach(([key,value]) => {
-        type = typeof value;
-        //只有不是循环引用才进行处理
-        if(value!==obj){
+	function _innerHandle(obj,parent = null){
+        let type = typeof obj, _parent = parent;
+        //如果是null，则返回null
+        if(obj === null) return null;
+        //如果是值类型，直接返回字符串形式
+        if(type !== 'object' && type !== 'function') return (type === 'string')?`"${obj}"`:`${obj}`;
+        //判断是有否循环引用
+        while(_parent){
+            //如果发现有循环引用
+            if(_parent.originParent === obj) return null;
+            //如果没有循环引用，继续向上查找，直到没有父级为止
+            _parent = _parent.parent;
+        }
+        //定义结果对象;是否是数组类型
+        let res = [], isArray = Object.prototype.toString.call(obj).slice(8,-1) === 'Array';
+        //循环自身可枚举键值对
+        Object.entries(obj).forEach(([key,value]) => {
+            type = typeof value;
+            //数组类型的每一项是不显示key的
             let keystyle = isArray?'':'"'+key+'":';
             switch(type){
-                //值是undefined,symbol和function，不处理
+                //值是undefined,symbol和function在数组里处理为null，其它情况忽略
                 case 'undefined':
                 case 'symbol':
                 case 'function':
+                    if(isArray) res.push('null');
                     break;
                 //值是object，则进行递归处理
                 case 'object':
-                    res.push(keystyle + JSONStringify((value && typeof value.toJSON === 'function')?value.toJSON():value));
+                	//如果对象有toJSON方法，处理的就是toJSON方法返回的值
+                    let child = (value && typeof value.toJSON === 'function')?value.toJSON():value;
+                    res.push(keystyle + _innerHandle(child, {
+                        originParent: obj,
+                        parent: parent
+                    }));
                     break;
-                //值是剩下的类型，返回对应的字符串
+                //值是剩下的类型
                 default:
-                    res.push(keystyle + value);
+                    res.push(keystyle + ((type === 'string')?'"'+value+'"':value));
                     break;
             }
-        }
-    });
-    return isArray?'['+String(res)+']':'{'+String(res)+'}';
+        });
+        return isArray?'['+String(res)+']':'{'+String(res)+'}';
+    }
+	return _innerHandle(obj);
 }
